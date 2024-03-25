fbc8baa4215df73a6097f2f76ddd198193cf7667
zacheliason <eliasonzach@gmail.com>
2024-03-25 15:29:42 -0600

    add group by protein

--- src/ManipulateFiles.py
+++ src/ManipulateFiles.py
@@ -5,72 +5,104 @@ import os
 
 
 def count_matching_characters(code1, code2):
-    arr1 = np.array(list(code1))
-    arr2 = np.array(list(code2))
+	arr1 = np.array(list(code1))
+	arr2 = np.array(list(code2))
 
-    # Find the minimum length of the two input strings
-    min_length = min(len(code1), len(code2))
+	# Find the minimum length of the two input strings
+	min_length = min(len(code1), len(code2))
 
-    # Compare the characters and create a boolean array
-    matches = arr1[:min_length] == arr2[:min_length]
+	# Compare the characters and create a boolean array
+	matches = arr1[:min_length] == arr2[:min_length]
 
-    # Count the True values (matches) in the boolean array
-    count = np.sum(matches)
+	# Count the True values (matches) in the boolean array
+	count = np.sum(matches)
 
-    return count
-
-
-def clean_output(path):
-    # Step 1: Read the TSV file into a DataFrame
-    df = pd.read_csv(path, sep='\t')
-
-    # Step 2: Extract the HLA type and MLE Probability columns
-    df['HLAletter'] = df['HLAtype'].str.extract(r'\((\w+\d*)\*.*\)')  # Extract HLA type (A, B, or C)
-    df[['HLA_type', 'HLA_code']] = df['HLAtype'].str.split(' ', expand=True)
-
-    df = df[df['HLAletter'].isin(['A', 'B', 'C'])]
+	return count
 
 
-    # Step 3: Filter out alleles based on a threshold probability
-    threshold = 0.001  # Adjust the threshold as needed
-    df_filtered = df[df['MLE_Probability'] >= threshold]
+def group_by_protein(df):
+	expanded_columns = df['HLA_code'].str.split(r'[\:\*]', n=5, expand=True)
 
-    # Step 4: Group alleles by their HLA type (A, B, or C)
-    groups = df_filtered.groupby('HLAletter')
+	# Rename the columns to match the desired output
+	expanded_columns.columns = ['HLAletter', 'group', 'protein', 'coding', 'noncoding']
 
-    # Step 5: Sort alleles within each group based on MLE Probability
-    sorted_groups = {key: group.sort_values(by='MLE_Probability', ascending=False) for key, group in groups}
+	# Fill any None values with Nones
+	expanded_columns = expanded_columns.where(expanded_columns.notnull(), None)
 
-    # Step 6: Select the top two alleles for each HLA type
-    hla_predictions = []
-    for group in sorted_groups.values():
-        top_values = group['MLE_Probability'].nlargest(2).values.tolist()
-        filtered_group = group[group['MLE_Probability'].isin(top_values)]
-        for mle_prob in filtered_group['MLE_Probability'].unique():
-            ng = filtered_group[filtered_group['MLE_Probability'] == mle_prob]
-            if len(ng) < 2:
-                hla_predictions.append(ng)
-            else:
-                # Define a custom sorting key function
-                def custom_sort_key(code):
-                    # Sort by the number of matching characters with other codes
-                    matching_counts = [count_matching_characters(code, other_code) for other_code in ng['HLA_code']]
-                    max_matching_count = sorted(matching_counts, reverse=True)[1]
+	# Concatenate the expanded columns with the original DataFrame
+	df = pd.concat([df, expanded_columns], axis=1)
+	df = df.sort_values(by=['MLE_Reads', 'HLA_code'], ascending=[False, True])
 
-                    # Second, sort alphabetically
-                    return (-max_matching_count, code)
+	grouped = df.groupby(['HLAletter', 'group', 'protein']).agg({
+		'HLAtype': 'first',
+		'HLA_type': 'first',
+		'HLA_code': 'first',
+		'MappedReads': 'sum',
+		'MappedProportion': 'sum',
+		'MLE_Reads': 'sum',
+		'MLE_Probability': 'sum'
+	}).reset_index().sort_values(by='MLE_Reads', ascending=False).drop(columns=['group', 'protein'])
 
-                # Sort the DataFrame based on the custom sorting key
-                df_sorted = ng.iloc[ng['HLA_code'].map(custom_sort_key).argsort()]
-                hla_predictions.append(df_sorted.head(1))
+	return grouped[['HLAtype', 'HLA_type', 'HLA_code', 'HLAletter', 'MappedReads', 'MappedProportion', 'MLE_Reads', 'MLE_Probability']]
 
-    hla_prediction_df = pd.concat(hla_predictions)
-    hla_prediction_df.index = hla_prediction_df['HLAtype']
-    hla_prediction_df = hla_prediction_df.drop(columns=['HLAletter', "HLA_code", "HLA_type", "HLAtype"])
 
-    output_dir, _ = os.path.split(path)
+def clean_output(path, group_by_p=True):
+	# Step 1: Read the TSV file into a DataFrame
+	df = pd.read_csv(path, sep='\t')
 
-    hla_prediction_df.to_csv(os.path.join(output_dir, "final_predictions.csv"), index=True)
+	# Step 2: Extract the HLA type and MLE Probability columns
+	df[['HLA_type', 'HLA_code']] = df['HLAtype'].str.split(' ', expand=True)
+	df['HLA_code'] = df['HLA_code'].apply(lambda x: re.sub(r'[\(\)]', '', x))
+
+	if group_by_p:
+		df = group_by_protein(df)
+	else:
+		df['HLAletter'] = df['HLAtype'].str.extract(r'\((\w+\d*)\*.*\)')  # Extract HLA type (A, B, or C)
+
+
+	df = df[df['HLAletter'].isin(['A', 'B', 'C'])]
+
+
+	# Step 3: Filter out alleles based on a threshold probability
+	threshold = 0.001  # Adjust the threshold as needed
+	df_filtered = df[df['MLE_Probability'] >= threshold]
+
+	# Step 4: Group alleles by their HLA type (A, B, or C)
+	groups = df_filtered.groupby('HLAletter')
+
+	# Step 5: Sort alleles within each group based on MLE Probability
+	sorted_groups = {key: group.sort_values(by='MLE_Probability', ascending=False) for key, group in groups}
+
+	# Step 6: Select the top two alleles for each HLA type
+	hla_predictions = []
+	for group in sorted_groups.values():
+		top_values = group['MLE_Probability'].nlargest(2).values.tolist()
+		filtered_group = group[group['MLE_Probability'].isin(top_values)]
+		for mle_prob in filtered_group['MLE_Probability'].unique():
+			ng = filtered_group[filtered_group['MLE_Probability'] == mle_prob]
+			if len(ng) < 2:
+				hla_predictions.append(ng)
+			else:
+				# Define a custom sorting key function
+				def custom_sort_key(code):
+					# Sort by the number of matching characters with other codes
+					matching_counts = [count_matching_characters(code, other_code) for other_code in ng['HLA_code']]
+					max_matching_count = sorted(matching_counts, reverse=True)[1]
+
+					# Second, sort alphabetically
+					return (-max_matching_count, code)
+
+				# Sort the DataFrame based on the custom sorting key
+				df_sorted = ng.iloc[ng['HLA_code'].map(custom_sort_key).argsort()]
+				hla_predictions.append(df_sorted.head(1))
+
+	hla_prediction_df = pd.concat(hla_predictions)
+	hla_prediction_df.index = hla_prediction_df['HLAtype']
+	hla_prediction_df = hla_prediction_df.drop(columns=['HLAletter', "HLA_code", "HLA_type", "HLAtype"])
+
+	output_dir, _ = os.path.split(path)
+
+	hla_prediction_df.to_csv(os.path.join(output_dir, "final_predictions.csv"), index=True)
 
 
 def calculate_match_percentage(obs, exp):
@@ -298,9 +330,25 @@ def score_output(output_dir, alleles_path, tool="HLA_EM"):
 
 		if tool == "HLA_EM":
 			final_predictions_path = [os.path.join(trial_dir, x) for x in trial_results if x.endswith("final_predictions.csv")]
+			if len(final_predictions_path) == 0:
+				print('Attempting to clean ' + trial_dir)
+				results_path = [os.path.join(trial_dir, x) for x in trial_results if x.endswith("results.tsv")]
+				if len(results_path) == 0:
+					print(f"Error: {trial_dir} does not contain a results.tsv file")
+					continue
+				else:
+					results_path = results_path[0]
+
+				clean_output(results_path)
+				final_predictions_path = [os.path.join(trial_dir, x) for x in os.listdir(trial_dir) if x.endswith("final_predictions.csv")]
+				if len(final_predictions_path) == 0:
+					print(f"Error: {trial_dir} does not contain a final_predictions.csv file")
+					continue
+
 			if len(final_predictions_path) != 1:
 				print(f"Error: {trial_dir} does not contain exactly one final_predictions.csv file")
 				continue
+
 			else:
 				final_predictions_path = final_predictions_path[0]
 
@@ -319,6 +367,12 @@ def score_output(output_dir, alleles_path, tool="HLA_EM"):
 	return pd.DataFrame(scores)
 
 
-# scores = score_output("/Users/zeliason/Desktop/hla-em/output", "/Users/zeliason/Desktop/hla-em/reference/allele_record.csv")
-#
-# print()
+# scores = score_output("/Users/zacheliason/Downloads/output_zipped_copy/output_single", "/Users/zacheliason/Downloads/hla-em/reference/allele_record.csv")
+# # print average scores
+# print(scores[['allele_group_score', 'protein_score', 'coding_score', 'noncoding_score']].mean())
+
+
+
+# scores = score_output("/Users/zacheliason/Downloads/output_zipped_copy/output_single", "/Users/zacheliason/Downloads/hla-em/reference/allele_record.csv", tool="Optitype")
+# print(scores[['allele_group_score', 'protein_score', 'coding_score', 'noncoding_score']].mean())
+# clean_output("/Users/zacheliason/Downloads/output_zipped_copy/output_single/trial_4/trial_4.results.tsv")
