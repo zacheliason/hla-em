62e0dc634622f065d2d3f21a4691b7470cb68d7f
zacheliason <eliasonzach@gmail.com>
2024-04-09 19:03:54 -0600

    add optitype single reads

--- evaluate_trials.sh
+++ evaluate_trials.sh
@@ -2,6 +2,7 @@
 
 ALLELE_RECORD_PATH="$(pwd)/reference/allele_record.csv"
 
+# Evaluate HLA-EM with paired reads
 OUTPUT_PATH="$(pwd)/output"
 NAME="HLA-EM_paired"
 
@@ -9,7 +10,16 @@ CSV_PATH="$(pwd)"/"$NAME"_scores.csv
 
 python3 src/evaluate_trials.py -n "$NAME" -o "$OUTPUT_PATH" -r "$ALLELE_RECORD_PATH" --outname "$CSV_PATH"
 
+# Evaluate Optitype with paired reads
 OUTPUT_PATH="$(pwd)/optitype_output_paired"
+NAME="optitype_paired"
+
+CSV_PATH="$(pwd)"/"$NAME"_scores.csv
+
+python3 src/evaluate_trials.py -n "$NAME" -o "$OUTPUT_PATH" -r "$ALLELE_RECORD_PATH" --outname "$CSV_PATH"
+
+# Evaluate Optitype with single reads
+OUTPUT_PATH="$(pwd)/optitype_output_single"
 NAME="optitype_single"
 
 CSV_PATH="$(pwd)"/"$NAME"_scores.csv
--- run_optitype.sh
+++ run_optitype.sh
@@ -8,6 +8,7 @@ PARENT_DIR=$(dirname "$SCRIPT_PATH")
 HOST_PATH=$PARENT_DIR
 VOLUME_PATH="/out"
 
+# Run Optitype with paired reads
 for TRIAL_NUM in {0..24}; do
   echo ""
   echo "working on ${TRIAL_NUM}"
@@ -21,6 +22,25 @@ for TRIAL_NUM in {0..24}; do
     fred2/optitype \
     -i "$TRIAL_DIR"/out.Unmapped.out.mate1.fastq \
        "$TRIAL_DIR"/out.Unmapped.out.mate2.fastq \
+    --verbose \
+    -r \
+    -o "$OUTPUT_DIR"/optitype_results_run_"$TRIAL_NUM"
+
+done
+
+# Run Optitype with single reads
+for TRIAL_NUM in {0..24}; do
+  echo ""
+  echo "working on ${TRIAL_NUM}"
+
+  TRIAL_DIR="$VOLUME_PATH"/reference/samples/trial_"$TRIAL_NUM"
+  OUTPUT_DIR="$VOLUME_PATH"/optitype_output_single/optitype_results_run_"$TRIAL_NUM"
+
+  docker run --rm \
+    -v "$HOST_PATH":"$VOLUME_PATH" \
+    -t \
+    fred2/optitype \
+    -i "$TRIAL_DIR"/out.Unmapped.out.mate1.fastq \
     --verbose \
     -r \
     -o "$OUTPUT_DIR"/optitype_results_run_"$TRIAL_NUM"
